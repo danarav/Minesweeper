7ffea2827633d9b23c78ad2e37391aea352db25e
danielariaz-avalos <dannyariaz45@gmail.com>
2022-04-07 19:52:47 -0700

    Created methods in Board class

--- board.rb
+++ board.rb
@@ -11,4 +11,35 @@ class board
 
     attr_reader :grid_size, :num_bombs
 
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def lost?
+        @grid.flatten.any? { |tile| tile.bombed? && tile.explored? }
+    end
+
+    private
+
+    def generate_board
+        @grid = Array.new(@grid_size) do |row|
+            Array.new(@grid_size) { |col| Tile.new(self, [row, col]) }
+        end
+
+        plant_bombs
+    end
+
+    def plant_bombs
+        total_bombs = 0
+        while total_bombs < @num_bombs
+            rand_pos = Array.new(2) { rand(@grid_size) }
+
+            tile = self[rand_pos]
+            next if tile.bombed?
+
+            tile.plant_bomb
+            total_bombs += 1
+    end
+
 end
